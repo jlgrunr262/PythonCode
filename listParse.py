86fc71ae2e51bad08b12b0d69372ac7dc9cddead
Jerry Gentry <jlgrunr@gmail.com>
2021-01-18 13:50:25 -0500

    How to use if statements with a List

---
+++ listParse.py
@@ -0,0 +1,12 @@
+numbers = [item for item in range(0,101) if item % 2 ==0]
+# for x in range(0,100):
+#     if x % 2 == 0:
+#         numbers.append
+print(numbers)
+
+odds = []
+for x in range(0,101):
+    if x % 3 == 0:
+        odds.append(x)
+
+print(odds)
