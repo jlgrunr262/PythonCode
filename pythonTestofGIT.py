b6b2151d30f4c9c23c203c0f134e60ee5815d5d3
jlgrunr262 <35612116+jlgrunr262@users.noreply.github.com>
2021-01-19 14:48:32 -0500

    Add files via upload

--- pythonTestofGIT.py
+++ pythonTestofGIT.py
@@ -1,1 +1,10 @@
-print('This is a test to see if I am replicating to the correct repo on GIT')
+sentence = ["This", "is", "to", "test", "if", "I", "am", "replicating", "to", "git"]
+altsent = ["This is to test if I am replicating to git"]
+
+for index, word in enumerate(sentence):
+    print("{}".format(word))
+# Exerything above here works (altsent isn't used)
+    if index % 2 == 0:
+        print(sentence[index])
+# 
+# newword = (int(sentence[index])
