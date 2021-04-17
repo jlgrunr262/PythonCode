0732e91cb9cbf8a21fa45d505a25c591ad2f8176
jlgrunr262 <jlgrunr@gmail.com>
2021-04-17 09:50:23 -0400

    AutoBoringStuff

---
+++ stringThing.py
@@ -0,0 +1,10 @@
+first_name = "Anya"
+last_name = "Gentry"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(full_name.title())
+print(full_name.lower())
+print(full_name.upper())
+print("{} {}".format(first_name, last_name))
+print("Hello, {}".format(full_name.title()))
+print("Hello...Hello, {} {}".format(first_name.title(), last_name.title()))
